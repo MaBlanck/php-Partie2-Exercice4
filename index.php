7fb3108378860cdd400827c75c9243c129eb7bad
Max <maxblanck@outlook.fr>
2020-12-10 10:03:01 +0100

    php Partie 2  Exercice 4

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-$magnitude = 1;
+$magnitude = rand(1,9);
 ?>
-
-
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
@@ -12,48 +10,40 @@ $magnitude = 1;
 </head>
 <body>
     <h1>Exercice 4 Partie 2</h1>
-    
     <p><?php
-        switch($magnitude)
-        {
+        switch ($magnitude) {
             case 1:
                 echo 'Micro-séisme impossible à ressentir.';
-            break;
-
+                break;
             case 2:
                 echo 'Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.';
-            break;
-
+                break;
             case 3:
                 echo 'Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.';
-            break;
-
+                break;
             case 4:
                 echo 'Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.';
-            break;
-
+                break;
             case 5:
                 echo 'Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.';
-            break;
-
+                break;
             case 6:
                 echo 'Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre.';
-            break;
-
+                break;
             case 7:
                 echo 'Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.';
-            break;
-
+                break;
             case 8:
                 echo 'Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.';
-            break;
-
+                break;
             case 9:
                 echo 'Séisme capable de tout détruire sur une très vaste zone.';
-            break;
+                break;
+            default: 
+                echo 'Vous êtes dead';
+                break;  
         };
         ?></p>
-
-
 </body>
+
 </html>
